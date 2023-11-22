afca54914409b87ab59da423d43d12e51b0db9bb
cdq73700 <cdq73700@gmail.com>
2023-11-23 02:12:39 +0900

    feat(frontend): 本文コンポーネント追加

---
+++ 01-LandingPage/frontend/components/MarkDown.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { Link } from '@mui/icons-material'
+import { Box, Button } from '@mui/material'
+
+const MarkDown = ({
+  title,
+  id,
+  children,
+}: {
+  title: string
+  id: string
+  children: React.ReactNode
+}) => {
+  return (
+    <>
+      <Box display={'flex'}>
+        <h1>{title}</h1>
+        <Button id={id} href={`#${id}`}>
+          <Link></Link>
+        </Button>
+      </Box>
+      {children}
+    </>
+  )
+}
+
+export default MarkDown
